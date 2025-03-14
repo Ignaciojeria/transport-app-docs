964e07187d680f8b1808cce8a7ce22ad034c40df
ignaciojeria <ignaciovl.j@gmail.com>
2025-03-14 15:02:42 -0300

    base project updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/transport-app-docs/",
   integrations: [starlight({
     title: 'Transport APP',
     social: {
